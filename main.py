404dddb858a42a13f130de26e202f1924868cf77
Viste <viste02@gmail.com>
2022-12-14 19:59:16 +0300

    fuck none in lastname

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ async def work_send_tax(message: types.Message):
     logging.info('info about message %s', message)
     logging.info('id of file %s', message.photo[-1].file_id)
     sender_name = message.chat.first_name
-    sender_lastname = message.chat.last_name
+    if message.chat.last_name is None:
+        sender_lastname = ' '
+    else:
+        sender_lastname = message.chat.last_name
     text = f"Мем прислал: {sender_name} {sender_lastname}"
     await bot.send_photo(channel, photo=message.photo[-1].file_id, caption=text)
     await message.reply("Спасибо за мем! Пока-пока")
@@ -29,7 +32,10 @@ async def work_send_demo(message: types.Message):
     logging.info('info about message %s', message)
     logging.info('id of file %s', message.video.file_id)
     sender_name = message.chat.first_name
-    sender_lastname = message.chat.last_name
+    if message.chat.last_name is None:
+        sender_lastname = ' '
+    else:
+        sender_lastname = message.chat.last_name
     content = message.video.file_id
     text = f"Мем прислал: {sender_name} {sender_lastname}"
     await bot.send_video(channel, video=content, caption=text)
