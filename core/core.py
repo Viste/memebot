c5108c8d1728f5ac2221965e6377af22e9ca6650
viste <viste02@gmail.com>
2024-08-11 13:44:11 +0300

    update deploy and etc

--- core/core.py
+++ core/core.py
@@ -4,17 +4,44 @@ from aiogram import types, F, Router
 from aiogram.filters.command import Command
 from main import memes
 from core.utils import config
+import requests
 
 logger = logging.getLogger("__name__")
 router = Router()
 channel = config.channel
 
 
+@router.message(Command(commands=['link']), F.chat.type == "private")
+async def link_account(message: types.Message):
+    user = message.from_user
+    profile_photos = await memes.get_user_profile_photos(user.id)
+    if profile_photos.total_count > 0:
+        photo_file_id = profile_photos.photos[0][-1].file_id
+        file = await memes.get_file(photo_file_id)
+        file_url = f"https://api.telegram.org/file/bot{memes.token}/{file.file_path}"
+
+        response = requests.post('https://dev-vlab.ru/update_telegram_profile', json={
+            'telegram_id': user.id,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'profile_picture': file_url
+        })
+
+        if response.status_code == 200:
+            await message.reply("Account linked successfully!")
+        else:
+            await message.reply("Failed to link your account.")
+    else:
+        await message.reply("You don't have a profile picture.")
+
+
 @router.message(Command(commands="start", ignore_case=True), F.chat.type == "private")
 async def start(message: types.Message):
     first_name = message.chat.first_name
 
-    await message.reply(f"Привет {first_name}, тут ты можешь отправить нам мемес. Принимаю только видосики и картинощки")
+    await message.reply(f"Привет {first_name}, тут ты можешь отправить нам мемес. Принимаю только видосики и картинощки\n"
+                        f"А еще связать свой аккаунт с сайтом dev-vlab.ru через команду /link")
 
 
 @router.message(F.content_type.in_({'photo'}), F.chat.type == "private")
