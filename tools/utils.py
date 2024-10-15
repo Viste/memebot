b442c3fdf63fdeaa93fc032b31013fe89f68953f
viste <viste02@gmail.com>
2024-10-15 13:02:08 +0300

    update anti spam
    Signed-off-by: viste <viste02@gmail.com>

--- tools/utils.py
+++ tools/utils.py
@@ -30,7 +30,7 @@ def load_config():
 
 config = load_config()
 
-SPAM_LINKS_REGEX = re.compile(r"(https?:\/\/)?([\w\-]+\.(xyz|binance|waxu|foxu|opensea))|[\w\u2060]", re.IGNORECASE)
+SPAM_LINKS_REGEX = re.compile(r"(https?:\/\/)?([\w\-]+\.(xyz|binance|waxu|foxu|opensea))", re.IGNORECASE)
 group_id = "-1001564920057"
 
 def is_spam(message: types.Message):
